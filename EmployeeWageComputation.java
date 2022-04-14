9f15ad9409a6ee7b41a8242b0787c4eba6d469ed
firozshaik36 <firozshaik0007@gmail.com>
2022-04-14 11:30:20 +0530

    REFACTORED:Calculating Employee Daily Wage.

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,18 +1,26 @@
 package com.bridgelabz.EmployeeWage;
-
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
 
         System.out.println("----- Welcome to Employee Wage Computation! -----");
 
-        int empPresesent = 1;
+        int IS_PRESENT = 1;
+        int EMP_WAGE_PER_HOUR = 20;
+
+        int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == empPresesent){
+        if(empCheck == IS_PRESENT){
             System.out.println("Employee is Present");
+            empHrs = 8;
         }
-        else
+        else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Hours : "+empHrs);
+        System.out.println("Emp Wage : "+empWage);
     }
 
 }
