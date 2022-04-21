375d432c00cbcd7089a449e19ceb5a0deba8e6b9
firozshaik36 <firozshaik0007@gmail.com>
2022-04-21 12:11:03 +0530

    REFACTORED:TO Save The Total Wage For Each Company.

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,19 +2,38 @@ package com.bridgelabz.EmployeeWage;
 
 
 public class EmployeeWageComputation {
-
+    /**
+     * Initializing Static Variables
+     */
     private static final int IS_FULL_TIME = 1;
     private static final int IS_PART_TIME = 2;
 
-    public static int calculateEmpWageForCompany(String company, int empRate, int numOFDays, int maxHours) {
+    private final String company;
+    private final int empRate;
+    private final int numOFDays;
+    private final int maxHours;
+    private int empWagePerMonth;
 
-        System.out.println("----- Welcome to Employee Wage Computation! -----");
-        int empHrs=0, empWagePerMonth = 0, totalWorkingdays=0, totalEmpHours = 0;
+    public EmployeeWageComputation(String company, int empRate, int numOFDays, int maxHours) {
+        this.company = company;
+        this.empRate = empRate;
+        this.numOFDays = numOFDays;
+        this.maxHours = maxHours;
+    }
+
+    public void calculateEmpWageForCompany() {
+
+        int empHrs=0, totalWorkingdays=0, totalEmpHours = 0;
 
         while(totalEmpHours <= maxHours && totalWorkingdays < numOFDays) {
 
             totalWorkingdays ++;
             int empType = (int)Math.floor(Math.random() * 10) % 3;
+
+            /**
+             * Adding Switch Statement
+             * to change empType control
+             */
 
             switch(empType) {
                 case IS_FULL_TIME:
@@ -36,14 +55,35 @@ public class EmployeeWageComputation {
             System.out.println("Emp Wage Per Day : "+empWagePerDay);
         }
 
-        System.out.println("Emp Wage Per Month for Company: "+company+" is : "+empWagePerMonth);
-        return empWagePerMonth;
+    }
+    public String toString() {
+        return "Emp Wage Per Month for Company " + company + " is : " + empWagePerMonth;
     }
 
     public static void main(String[] args) {
-        calculateEmpWageForCompany("Firoz", 20, 2, 10);
-        calculateEmpWageForCompany("Shaik", 10, 4, 20);
+        /**
+         * Adding Welcome message to understand what program it is
+         */
+        System.out.println("----- Welcome to Employee Wage Computation! -----");
 
+        /**
+         * Creating Reference
+         *
+         */
+
+        EmployeeWageComputation firoz = new EmployeeWageComputation("FIROZ", 20, 2, 10);
+        EmployeeWageComputation shaik = new EmployeeWageComputation("SHAIK", 40, 5, 20);
+
+        /**
+         * Calling the Method by reference
+         */
+
+        firoz.calculateEmpWageForCompany();
+        System.out.println(firoz);
+        System.out.println();
+
+        shaik.calculateEmpWageForCompany();
+        System.out.println(shaik);
     }
 
 }
